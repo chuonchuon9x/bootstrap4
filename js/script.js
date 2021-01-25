93c0af550610ac3df6322f6d5049dc2e0b522a60
chuonchuon9x <chuonchuon9x@gmail.com>
2021-01-25 12:25:20 +0700

    update code script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var menu = $("#wrap-header");
+
     $(window).scroll(function(event) {
         var height = $(window).scrollTop();;
         if (height > 100) {
@@ -8,4 +9,28 @@ $(document).ready(function() {
             $(menu).removeClass("bg-header");
         }
     });
+
+    //Zoom Footer
+    function zoomfooter() {
+        var window = document.documentElement.clientHeight;
+        // var window = $(document.documentElement).innerHeight();
+        console.log(window);
+        var banner = $("#wrap-banner").outerHeight();
+        var service = $("#wrap-service").outerHeight();
+        var details = $("#wrap-details").outerHeight();
+        var footer = $("#wrap-footer");
+        var totalweb1;
+        totalweb1 = banner + service + details;
+        console.log(totalweb1);
+        if (window > totalweb1) {
+            $(footer).addClass("fixwindow");
+        } else {
+            $(footer).removeClass("fixwindow");
+        }
+    }
+    // End zoom footer
+    zoomfooter();
+    window.onresize = function() {
+        zoomfooter();
+    }
 });
